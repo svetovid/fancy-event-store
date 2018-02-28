e3fb2bafa41026ffe9c887cc5e1bae3b398e0aaf
svetovid <divotevs@gmail.com>
2018-02-28 14:29:01 +0200

    Add files via upload

---
+++ kickstart.bat
@@ -0,0 +1,8 @@
+start Lighthouse\bin\Debug\Lighthouse.exe -ipaddress:"127.0.0.1" -port:4053
+start Lighthouse\bin\Debug\Lighthouse.exe -ipaddress:"127.0.0.1" -port:4044
+start Hackaton.EventReader\bin\Debug\Hackaton.EventReader.exe
+start Hackaton.EventReader\bin\Debug\Hackaton.EventReader.exe
+start Hackaton.EventStore\bin\Debug\Hackaton.EventStore.exe
+start Hackaton.EventStore\bin\Debug\Hackaton.EventStore.exe
+start Hackaton.Journaling\bin\Debug\Hackaton.Journaling.exe
+PAUSE
---
+++ shutdown.bat
@@ -0,0 +1,5 @@
+taskkill /IM Hackaton.Journaling.exe
+taskkill /IM Hackaton.EventStore.exe
+taskkill /IM Hackaton.EventReader.exe
+taskkill /IM Lighthouse.exe
+PAUSE
